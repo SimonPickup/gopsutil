535b10f412cab2cc400f4702bfc4b11f7e5ed4de
Simon Pickup <simon@pickupinfinity.com>
2020-10-11 07:59:09 +0100

    More build constraints for making ios unsupported

--- disk/disk_unix.go
+++ disk/disk_unix.go
@@ -1,4 +1,5 @@
 // +build freebsd linux darwin
+// +build !ios
 
 package disk
 
--- disk/iostat_darwin.c
+++ disk/iostat_darwin.c
@@ -1,3 +1,5 @@
+// +build !ios
+
 // https://github.com/lufia/iostat/blob/9f7362b77ad333b26c01c99de52a11bdb650ded2/iostat_darwin.c
 #include <stdint.h>
 #include <CoreFoundation/CoreFoundation.h>
